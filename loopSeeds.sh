0d6a3a21fe3f322adfdf3ec7a99b071c4de75183
Z0L0X <yaseene937@gmail.com>
2024-12-23 15:57:15 +0300

    Update loopSeeds.sh

--- loopSeeds.sh
+++ loopSeeds.sh
@@ -1,5 +1,5 @@
 # create a for loop to run the seed executable 100 times with 12 threads each
-
+./loopSeeds.sh
 for i in $(seq 1 1 100); do
   for j in $(seq 1 1 16); do
     ./seed >output$j.txt &
